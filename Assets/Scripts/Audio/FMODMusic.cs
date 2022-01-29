90ac1555b6124b49a1a890e0d228703ca99d8137
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-01-29 20:13:13 +0100

    music FMOD

---
+++ Assets/Scripts/Audio/FMODMusic.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FMODMusic : MonoBehaviour
+{
+    private const string VELOCITY_NAME = "Velocidad";
+
+    private FMOD.Studio.EventInstance instance;
+    [SerializeField]
+    private FMODUnity.EventReference fmodEvent;
+    [SerializeField]
+    private SpeedConfig _speedConfig;
+    [SerializeField]
+    private FMODUnity.EditorParamRef _parameter;
+
+
+    private int _velocity;
+    private int _mod;
+    private float _musicTracksAmount;
+    
+    void Start()
+    {
+        GameService.Instance.OnChangeState += OnChangedState;
+        OnChangedState();
+   
+    }
+
+    private void OnChangedState()
+    {
+        if (GameService.Instance.State == GameService.GameState.Playing)
+        {
+            instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+            _musicTracksAmount = _parameter.Max;
+            _mod = Mathf.RoundToInt(_speedConfig.maxSpeed / _musicTracksAmount);
+            ClockService.Instance.OnUpdateEvent += CustomUpdate;
+
+            StartMusic();
+
+        }
+        else if (GameService.Instance.State == GameService.GameState.GameOver)
+        {
+            StopMusic();
+            ClockService.Instance.OnUpdateEvent -= CustomUpdate;
+        }
+    }
+
+    private void CustomUpdate(float deltaTime)
+    {
+        
+        if (_velocity == ((Mathf.RoundToInt(GameService.Instance.Speed) / _mod) + 1))
+        {
+            return;
+        }
+        _velocity = ((Mathf.RoundToInt(GameService.Instance.Speed) / _mod) + 1);
+        SetMusicTrack();
+    }
+
+    private void SetMusicTrack()
+    {
+        instance.setParameterByName(VELOCITY_NAME, _velocity);
+    }
+
+    private void StartMusic()
+    {
+        instance.start();
+    }
+
+    public void StopMusic()
+    {
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
+
